21124dab1835b0b495ef7480580e7e8868b2a998
karimah2004 <130722594+karimah2004@users.noreply.github.com>
2025-02-13 19:19:02 -0500

    Moving files over

---
+++ LSP_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	
+	/**
+	 * Karimah Mohammed 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
