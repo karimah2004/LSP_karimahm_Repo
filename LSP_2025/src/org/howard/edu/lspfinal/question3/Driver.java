9c40fb935e7b45fa96bad9932851c2558f20d3e3
karimah2004 <130722594+karimah2004@users.noreply.github.com>
2025-04-24 20:11:10 -0400

    added some more comments

--- LSP_2025/src/org/howard/edu/lspfinal/question3/Driver.java
+++ LSP_2025/src/org/howard/edu/lspfinal/question3/Driver.java
@@ -5,11 +5,14 @@ package org.howard.edu.lspfinal.question3;
  */
 public class Driver {
     public static void main(String[] args) {
+    	
+    	//create and generate the sales report
         ReportStructure salesReport = new SalesReport();
         salesReport.generateReport();
 
         System.out.println();
 
+        //create and generate the inventory report
         ReportStructure inventoryReport = new InventoryReport();
         inventoryReport.generateReport();
     }
